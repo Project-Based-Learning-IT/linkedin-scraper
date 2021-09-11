56d2262a091b981522b0fb90d583f0af779c8f36
Siddhesh Kothadi <55179845+siddheshkothadi@users.noreply.github.com>
2021-09-11 17:54:39 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -19,17 +19,21 @@ elementID.send_keys('project@123')
 elementID.submit()
 
 def linkedin_scrape(linkedin_urls):
-	SCROLL_PAUSE_TIME = 5
+	SCROLL_PAUSE_TIME = 2
 
 	# Get scroll height
-	last_height = _DRIVER_CHROME.execute_script("return document.body.scrollHeight")
+	last_height = int(_DRIVER_CHROME.execute_script("return document.body.scrollHeight"))
 	profiles = []
 	for url in linkedin_urls:
 
 		_DRIVER_CHROME.get(url)
 		# sleep(5)
 
-		_DRIVER_CHROME.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+		# _DRIVER_CHROME.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+		for i in range(1, last_height, 180): 
+			_DRIVER_CHROME.execute_script("window.scrollTo(0, {});".format(i))
+			time.sleep(0.5)
+
 		time.sleep(SCROLL_PAUSE_TIME)
 
 		# Calculate new scroll height and compare with last scroll height
@@ -45,20 +49,21 @@ def linkedin_scrape(linkedin_urls):
 		if name:
 			name = name.strip()
 
-		position = selector.xpath('//*[starts-with(@class, "text-body-medium")]/text()').extract_first()
-		headline = ""
-		company = ""
+		# position = selector.xpath('//*[starts-with(@class, "text-body-medium")]/text()').extract_first()
+		# headline = ""
+		# company = ""
 
-		if position:
-			headline = position.strip()
-			company = headline[headline.find('at')+3:]
-			position = position.strip()
-			position = position[0:position.find(' at ')]
+		# if position:
+		# 	headline = position.strip()
+		# 	company = headline[headline.find('at')+3:]
+		# 	position = position.strip()
+		# 	position = position[0:position.find(' at ')]
 
 		#locate link to expand skills
 		show_more_skills_button = _DRIVER_CHROME.find_element_by_class_name("pv-skills-section__chevron-icon")
 		#expand
-		show_more_skills_button.click()
+		# show_more_skills_button.click()
+		_DRIVER_CHROME.execute_script("arguments[0].click();", show_more_skills_button)
 
 		skills = _DRIVER_CHROME.find_elements_by_xpath("//*[starts-with(@class,'pv-skill-category-entity__name-text')]")
 
@@ -68,12 +73,12 @@ def linkedin_scrape(linkedin_urls):
 			# print(skill.text)
 			skill_set.append(skill.text)
 
-		profiles.append([name, headline, position, company, url])
-		print(f'{len(profiles)}: {name}, {headline}, {position}, {company}, {url}, {skill_set}')
+		profiles.append([name, skill_set])
+		print(f'{len(profiles)}: {name}, {url}, {skill_set}')
+
+		# profiles.append([name, headline, position, company, url])
+		# print(f'{len(profiles)}: {name}, {headline}, {position}, {company}, {url}, {skill_set}')
 	return profiles
 
-urls = ['https://www.linkedin.com/in/siddhesh-kothadi/']
+urls = ['https://www.linkedin.com/in/mayank-sahu-12238b191/', 'https://www.linkedin.com/in/atharva-parikh-ap07/', 'https://www.linkedin.com/in/sidhant-khamankar/']
 linkedin_scrape(urls)
-
-# Sample output:
-# 	1: Siddhesh Kothadi, Android Developer at DSC VIIT Pune, Android Developer, DSC VIIT Pune, https://www.linkedin.com/in/siddhesh-kothadi/, ['C++', 'Kotlin', 'Android Development', 'JavaScript', 'React.js', 'Node.js', 'Nuxt.js', 'Python', 'MongoDB', 'Cascading Style Sheets (CSS)', 'Figma', 'HTML', 'Github', 'Google Cloud Platform (GCP)']
